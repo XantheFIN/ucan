04ae08ed90ea6d46295b6f1751e5df36528e5966
barnet <dev001@codeskin.com>
2017-12-28 15:46:26 -0500

    Update method description.

--- src/can/CanPort.h
+++ src/can/CanPort.h
@@ -37,59 +37,75 @@ class CanPort {
 public:
 
 	/**
-	 * Queues message for transmission.
-	 * @param aMsg message to be queued
-	 * @param aTimeoutMs how long to wait if transmit queue is full
-	 * @return true if successfully queued
+	 * Send message.
+	 * Queues CAN message into transmit buffer. A transaction id is associated with
+	 * the pending transmission, and can be used in conjunction with getSentMessage()
+	 * to subsequently confirm a successful transmission.
+	 *
+	 * @param aMsg CAN message
+	 * @param aTransactionId id associated with transmission (set to nullptr if not used)
+	 * @return true if successful
 	 */
 	virtual bool sendMessage(SharedCanMessage aMsg, uint16_t *aTransactionId) = 0;
 
 	/**
-	 * Returns number of messages in receive buffer.
-	 * @return number of messages in buffer
+	 * Get number of messages in receive buffer.
+	 * Received messages are retrieved by means of getReceivedMessage().
+	 * @param number of messages in receive buffer
 	 */
 	virtual int numReceivedMessagesAvailable() = 0;
 
 	/**
-	 * Retrieves message from receive buffer.
-	 * @param aMsg message received
-	 * @param aTimeoutMs how long to wait if receive queue is empty
-	 * @return true of message successfully dequeued
+	 * Retrieve message from receive buffer.
+	 *
+	 * @param aMsg object to store received message
+	 * @param aTimeoutMs time to wait for received message
+	 * @return true when valid message is returned by function
 	 */
 	virtual bool getReceivedMessage(SharedCanMessage& aMsg, uint32_t aTimeoutMs) = 0;
 
 	/**
-	 * Returns number of messages in transmit acknowledge buffer
-	 * @return number of messages in buffer
+	 * Get number of successfully sent messages stored in transmit acknowledge buffer.
+	 * Messages transmitted are stored in the transmit acknowledge buffer and can
+	 * be retrieved my means of getSentMessage().
+	 * @param number of transmit acknowledge buffer
 	 */
 	virtual int numSentMessagesAvailable() = 0;
 
 	/**
-	 * Retrieves message from transmit acknowledge buffer.
-	 * @param aMsg message transmitted
-	 * @param aTimeoutMs how long to wait if transmit acknowledge queue is empty
-	 * @return true of message successfully dequeued
+	 * Retrieve message from transmit acknowledge buffer.
+	 * If a non-zero transaction id is specified, the function will block until
+	 * the message corresponding to the transaction id as been transmitted, and
+	 * stored in the transmit acknowledge buffer, or until the specified timeout
+	 * expires.
+	 *
+	 * @param aMsg object to store transmitted message
+	 * @param aTransactionId desired transaction id
+	 * @param aTimeoutMs time to wait for received message
+	 * @return true when valid message is returned by function
 	 */
 	virtual bool getSentMessage(SharedCanMessage& aMsg, uint16_t aTransactionId, uint32_t aTimeoutMs) = 0;
 
 	/**
-	 * Getter for error code
-	 * @return error
+	 *  Getter for error code corresponding to last error that occurred.
+	 *  Use getErrorDescription() to obtain textual description of error code.
+	 *  @return error code
 	 */
 	virtual int getErrorCode() = 0;
 
 	/**
-	 * Retrieves error description and secondary error code
-	 * @param aErrorCode code for which description is requested
-	 * @param aErrorString string for error description
-	 * @param aSecondaryCode secondary error code
+	 * Convert numerical error code to error description.
+	 *
+	 * @param aErrorCode error code obtained from getErrorCoder()
+	 * @param aErrorString textual error description
+	 * @param aSecondaryCode secondary error code (if applicable)
 	 */
 	virtual void getErrorDescription(int aErrorCode, std::string &aErrorString, int32_t *aSecondaryCode) = 0;
 
 	/**
-	 * Retrieves CAN error counters (according to CAN standard)
-	 * @param aTxErrorCounter transmit error counter
-	 * @param aRxErrorCounter receiver error counter
+	 *  Return CAN error counters.
+	 *  @param aTxErrorCounter number of transmit errors
+	 *  @param aRxErrorCounter number of receive errors
 	 */
 	virtual void getErrorCounters(int *aTxErrorCounter, int *aRxErrorCounter) = 0;
 };
